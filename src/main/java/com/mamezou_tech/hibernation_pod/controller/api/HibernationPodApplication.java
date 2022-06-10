1d44dca0b95a62a97f02b432d477c8b6286fe14a
Shigeki Shoji <shigeki-shoji@mamezou.com>
2022-06-11 07:48:32 +0900

    1st

---
+++ src/main/java/com/mamezou_tech/hibernation_pod/controller/api/HibernationPodApplication.java
@@ -0,0 +1,13 @@
+package com.mamezou_tech.hibernation_pod.controller.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernationPodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernationPodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mamezou_tech/hibernation_pod/controller/api/HibernationPodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mamezou_tech.hibernation_pod.controller.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernationPodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
